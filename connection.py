e12c21baaf47835cc8d9782716f44db868fcb3b6
Victor Hernandez <vik407@gmail.com>
2020-03-30 15:20:53 +0000

    two

--- connection.py
+++ connection.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 # PLD Session
+# Table structure one (one varchar(10), two smallint)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-#
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
-Base = declarative_base
+Base = declarative_base()
 
 engine = create_engine('sqlite:///orm.db')
 connection = engine.connect()
 
+class One(Base):
+    __tablename__ = 'one'
+
+    one = Column(String(10),
+                nullable=False,
+                default='papi'
+                )
+    two = Column(Integer,
+                
+                )
