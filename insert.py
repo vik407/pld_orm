83fa4c2300495c5c5687e20c6e64e2c668453371
Victor Hernandez <vik407@gmail.com>
2020-03-30 15:56:52 +0000

    insert two

--- insert.py
+++ insert.py
@@ -4,7 +4,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Table, MetaData
 
 Base = declarative_base()
 
@@ -31,15 +31,28 @@ class One(Base):
     def __str__(self):
         return self.one + " " + str(self.two)
 
+# Create a new table
+metadata = MetaData()
+Two = Table('two', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('content', String(16), nullable=False)
+)
 
 # Link Models to get data
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+# Print content from table ONE
 for one in session.query(One).all():
     # Using def __str__ can use -> print(one)
     print("Field One:{} - Field Two:{}".format(one.one, one.two))
+
+# Insert content to table TWO
+insertQuery = Two.insert().values(id=1, content="Vamos Tio")
+
+# Print SQL Alchemy Query
+print(insertQuery)
 
 session.close()
 connection.close()
